5cec457dc5dcb380d1fa6266e1c4430fb7547d43
NgocDungNguyen <s3978535@rmit.edu.vn>
2026-01-15 17:29:28 +0700

    feat: update extension to use production API URL

--- extension/popup.js
+++ extension/popup.js
@@ -136,7 +136,7 @@ document.getElementById('confirmYes').addEventListener('click', async () => {
 
     try {
         // Send to API
-        const response = await fetch("http://localhost:8000/analyze/full_scan", {
+        const response = await fetch("https://vncontentguard-pro.onrender.com/analyze/full_scan", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
